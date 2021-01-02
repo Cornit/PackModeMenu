c2982c2e3f1d57be29b2dcd686985c118fd37f7b
TCreopargh <TCreopargh@gmail.com>
2021-01-03 00:10:06 +0800

    Allows pack makers to add aliases and descriptions to the packmodes

--- src/main/java/me/illgilp/packmodemenu/gui/ConfigScreen.java
+++ src/main/java/me/illgilp/packmodemenu/gui/ConfigScreen.java
@@ -1,5 +1,6 @@
 package me.illgilp.packmodemenu.gui;
 
+import com.mojang.realmsclient.gui.ChatFormatting;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.gui.GuiButton;
 import net.minecraft.client.gui.GuiScreen;
@@ -9,7 +10,6 @@ import net.minecraftforge.common.config.Property;
 import net.minecraftforge.fml.client.config.GuiButtonExt;
 
 import java.io.File;
-import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Arrays;
 
@@ -36,7 +36,7 @@ public final class ConfigScreen extends GuiScreen {
         configuration = new Configuration(new File("config/packmode.cfg"));
         configuration.load();
         packMode = configuration.get("general", "packMode", "normal");
-        acceptedModes = configuration.get("general", "acceptedModes", new String[] {"normal", "expert"});
+        acceptedModes = configuration.get("general", "acceptedModes", new String[]{"normal", "expert"});
 
     }
 
@@ -48,10 +48,10 @@ public final class ConfigScreen extends GuiScreen {
         }
         switchBtn = new GuiButtonExt(0, width / 2 - 100, height / 6, getButtonText());
         buttonList.add(switchBtn);
-        saveBtn = new GuiButtonExt(1, this.width / 2 - 155, this.height / 6 + 120 - 6, 150, 20, I18n.format("packmodemenu.options.save"));
+        saveBtn = new GuiButtonExt(1, this.width / 2 - 155, this.height / 6 + 160 - 6, 150, 20, I18n.format("packmodemenu.options.save"));
         saveBtn.enabled = !mode.equals(defaultMode);
         this.buttonList.add(saveBtn);
-        this.buttonList.add(new GuiButtonExt(2, this.width / 2 + 5, this.height / 6 + 120 - 6, 150, 20, I18n.format("packmodemenu.options.cancel")));
+        this.buttonList.add(new GuiButtonExt(2, this.width / 2 + 5, this.height / 6 + 160 - 6, 150, 20, I18n.format("packmodemenu.options.cancel")));
 
 
         super.initGui();
@@ -59,23 +59,35 @@ public final class ConfigScreen extends GuiScreen {
     }
 
     private String getButtonText() {
-        return I18n.format("packmodemenu.options.pack_mode") + ": " + (!mode.equals(defaultMode) ? "§a" : "") + mode.toUpperCase() + (!mode.equals(defaultMode) ? "" : " (" + I18n.format("packmodemenu.options.current") + ")");
+        String translatedModeName;
+        if (I18n.hasKey("packmodemenu.packmode.name." + mode)) {
+            translatedModeName = I18n.format("packmodemenu.packmode.name." + mode);
+        } else {
+            translatedModeName = mode.toUpperCase();
+        }
+        return I18n.format("packmodemenu.options.pack_mode") + ": " + (!mode.equals(defaultMode) ? "§a" : "") + translatedModeName + (!mode.equals(defaultMode) ? "" : " (" + I18n.format("packmodemenu.options.current") + ")");
     }
 
     @Override
     public void drawScreen(int mouseX, int mouseY, float partialTicks) {
         drawDefaultBackground();
-        this.drawCenteredString(this.fontRenderer, I18n.format("packmodemenu.options.pack_mode"), this.width / 2, 15, 16777215);
-
-        if (!mode.equals(defaultMode)) {
-            this.drawCenteredString(this.fontRenderer, I18n.format("packmodemenu.options.warning.restart.line1"), this.width / 2, this.height / 6 + 75 - 6, 0xFF5555);
-            this.drawCenteredString(this.fontRenderer, I18n.format("packmodemenu.options.warning.restart.line2"), this.width / 2, this.height / 6 + 75 - 6 + 10, 0xFF5555);
+        this.drawCenteredString(this.fontRenderer, I18n.format("packmodemenu.options.pack_mode"), this.width / 2, 15, 0xffffff);
+        StringBuilder textMultiline = new StringBuilder();
+        if (I18n.hasKey("packmodemenu.packmode.desc." + mode)) {
+            textMultiline.append(I18n.format("packmodemenu.packmode.desc." + mode).replace("\\n", "\n")).append("\n\n");
         }
+        if (!mode.equals(defaultMode)) {
+            String[] warningText = I18n.format("packmodemenu.options.warning.restart").split("\\\\n");
+            for (String line : warningText) {
+                textMultiline.append(ChatFormatting.RED).append(line).append("\n");
+            }
+        }
+        this.drawMultilineString(textMultiline.toString(), this.width / 2, this.height / 6 + 50 - 6, 0xffffff);
         super.drawScreen(mouseX, mouseY, partialTicks);
     }
 
     @Override
-    protected void actionPerformed(GuiButton button) throws IOException {
+    protected void actionPerformed(GuiButton button) {
         if (button.id == switchBtn.id) {
             mode = acceptedModes.getStringList()[(new ArrayList<>(Arrays.asList(acceptedModes.getStringList())).lastIndexOf(mode) + 1) % acceptedModes.getStringList().length];
             switchBtn.displayString = getButtonText();
@@ -86,6 +98,15 @@ public final class ConfigScreen extends GuiScreen {
             Minecraft.getMinecraft().displayGuiScreen(this.lastScreen);
         } else {
             Minecraft.getMinecraft().displayGuiScreen(this.lastScreen);
+        }
+    }
+
+    public void drawMultilineString(String textMultiline, int x, int y, int color) {
+        String[] texts = textMultiline.split("\\n");
+        int offset = 0;
+        for (String text : texts) {
+            this.drawCenteredString(this.fontRenderer, text, x, y + offset, color);
+            offset += 10;
         }
     }
 }
